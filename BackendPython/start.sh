dba5c622de79cfb5033403b667a11f37acafde11
HARISH GOWDA N <160123683+harishgowda120@users.noreply.github.com>
2025-10-14 09:30:45 +0530

    Update start.sh

--- BackendPython/start.sh
+++ BackendPython/start.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-# Upgrade pip and build tools
+# Upgrade build tools
 pip install --upgrade pip setuptools wheel
+
+# Install dependencies
+pip install -r requirements.txt
 
 # Start Flask app
 python app.py
