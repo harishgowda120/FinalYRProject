b4ce5579e2a58e5b43cf80c3465e369f99386c27
HARISH GOWDA N <160123683+harishgowda120@users.noreply.github.com>
2025-10-15 10:49:36 +0530

    Update Signup.jsx

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -73,7 +73,7 @@ export default function Signup() {
     }
 
     try {
-      const res = await axios.post("http://localhost:5000/api/users/signup", formData);
+      const res = await axios.post("https://finalyrproject-2.onrender.com/api/users/signup", formData);
       console.log("✅ Signup Success:", res.data);
       setMessage("✅ Signup successful! Redirecting to login...");
 
@@ -330,4 +330,5 @@ export default function Signup() {
       </div>
     </div>
   );
+
 }
